92eddf5f67f3d84dfedf6b9ad1ad7bb730151a1a
yangchao <yangchao@renrenche.com>
2017-04-20 21:26:48 +0800

    131 ac

--- 101-150/131. Palindrome Partitioning (M)/131. Palindrome Partitioning (M).py
+++ 101-150/131. Palindrome Partitioning (M)/131. Palindrome Partitioning (M).py
@@ -11,7 +11,50 @@ class Solution(object):
         :type s: str
         :rtype: List[List[str]]
         """
-        print '--------'
         l = len(s)
         if l == 0:
             return []
+
+        _str = ['#']
+        for x in s:
+            _str.append(x)
+            _str.append('#')
+
+        group = [[] for x in s]
+        for i in range(1, l * 2):
+            left = i - 1
+            right = i + 1
+            while left >= 0 and right <= 2 * l:
+                if _str[left] == _str[right]:
+                    if _str[left] == '#':
+                        sub_str = ''.join(s[left / 2: right / 2])
+                        group[left / 2].append(sub_str)
+                    left -= 1
+                    right += 1
+                else:
+                    break
+        result = []
+
+        def find_match(start, partition, child):
+            for item in group[start]:
+                reach = len(item)+start
+                if reach < l:
+                    find_match(reach, partition+child, [item])
+                elif reach == l:
+                    result.append(partition+child+[item])
+        find_match(0, [], [])
+
+        return result
+
+sl = Solution()
+
+print sl.partition('aba')
+print sl.partition('')
+print sl.partition('a')
+print sl.partition('aa')
+print sl.partition('abacdfgdcaba')
+print sl.partition('aba121ba')
+print sl.partition('aba121aba')
+print sl.partition('waabwswfd')
+print sl.partition('cabacabac')
+print sl.partition('caaaac')
